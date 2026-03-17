067dc23cbee5944e951805ad0ef4bd5fda8f3c1d
shap0011 <shap0011@algonquinlive.com>
2026-03-17 11:17:08 -0400

    test husky

---
+++ infrastructure/main.tf
@@ -0,0 +1,37 @@
+# terraform {
+#   required_version = ">= 1.5.0"
+
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 3.117"
+#     }
+#   }
+# }
+
+# provider "azurerm" {
+#   features {}
+# }
+
+# resource "azurerm_resource_group" "rg" {
+#   name     = "rg-cst8918-a09"
+#   location = "Canada Central"
+# }
+
+# resource "azurerm_storage_account" "storage" {
+#   name                     = "cst8918a09storage01"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = azurerm_resource_group.rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
+
+terraform {
+  required_version = ">= 1.5.0"
+}
+
+resource "terraform_data" "example" {
+  input = "cst8918-a09"
+}
+
+#--
